36a8afdf5fd16cb721438269308461f9f51a94f3
JordanEGarcia <jordaneddygarcia@gmail.com>
2020-12-05 13:26:12 -0600

    starting on implimenting order

---
+++ SimpleStore/StoreApp/StoreApp.Webapp/Controllers/ShoppingCartController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoreApp.DataAccess.Repositores;
+using StoreApp.Webapp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreApp.Webapp.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly IRepository _repository;
+
+        public ShoppingCartController(IRepository repository)
+        {
+            _repository = repository;
+        }
+        public IActionResult Index()
+        {
+            String cartItems = HttpContext.Session.GetString("Cart");
+            var data = new Serialize(cartItems, _repository);
+                return View();
+        }
+    }
+}
